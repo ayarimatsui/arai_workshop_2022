09ed5c82f37c5e5c9940d703a1a51110b0ac0ce4
ayarimatsui <ayanapo9846@gmail.com>
2022-07-08 16:35:29 +0900

    test commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDaQvfAm1Emc0zu73HgeR-Uj0UjdBYZjbw",
-	authDomain: "arai-workshop-2022.firebaseapp.com",
-	projectId: "arai-workshop-2022",
-	storageBucket: "arai-workshop-2022.appspot.com",
-	messagingSenderId: "159582546812",
-	appId: "1:159582546812:web:162ffa2dda8bed5a1092f9",
-};
+	apiKey: "AIzaSyDjX7lW5Zg27EpcQ6U8Uu800AIAYDFAuYk",
+	authDomain: "workshop-test-e6ef8.firebaseapp.com",
+	projectId: "workshop-test-e6ef8",
+	storageBucket: "workshop-test-e6ef8.appspot.com",
+	messagingSenderId: "981801377261",
+	appId: "1:981801377261:web:372f9210a2e9c2bcbc1be4"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
